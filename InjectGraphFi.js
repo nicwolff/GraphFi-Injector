7bcd0403f05af54a0cf416b4596aca59c50bb576
Nic Wolff <nwolff@hearst.com>
2014-06-05 21:24:54 -0400

    Initial commit

---
+++ InjectGraphFi.js
@@ -0,0 +1,4 @@
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.src = 'https://raw.githubusercontent.com/yourcelf/marksism/master/src/graphfi/bm.js';
+document.getElementsByTagName('head')[0].appendChild(s);
