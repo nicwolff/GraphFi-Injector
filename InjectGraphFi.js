69a134bc8e6775a8f5f738e91ccee1c3b6eef2d3
Nic Wolff <nic@angel.net>
2018-01-10 16:39:06 -0500

    Change download URL

--- InjectGraphFi.js
+++ InjectGraphFi.js
@@ -1,4 +1,4 @@
 var s = document.createElement('script');
-s.type = 'text/javascript';
-s.src = 'https://raw.githubusercontent.com/yourcelf/marksism/master/src/graphfi/bm.js';
+s.type = 'application/javascript';
+s.src = 'https://yerk.org/graphfi/bm.js';
 document.getElementsByTagName('head')[0].appendChild(s);
